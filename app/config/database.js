59e82ccb73621f0c96a4052c8a9d41fddebd7dee
[FrancoMassola] <frann.massola@gmail.com>
2020-12-05 14:11:52 -0300

    add database connection settings

---
+++ app/config/database.js
@@ -0,0 +1,18 @@
+//database connection settings
+const mongoose = require('mongoose');
+const app_config = require('./config');
+
+//the database connection module is exported
+module.exports = {
+    connection: null,
+    connect: function(){
+        if(this.connection) return this.connection;
+        //if the connection is null
+        return mongoose.connect(app_config.DB).then(connection =>{
+            //a connection is assigned
+            this.connection = connection;
+            console.log('successful connection to the database');
+        //in case an error is generated in the connection, it must be captured
+        }).catch(error =>console.log(error));
+    }
+}
