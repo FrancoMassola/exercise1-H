008860e5e41aee398e1f5697af1aba6e65cc1c9e
[FrancoMassola] <frann.massola@gmail.com>
2020-12-05 21:01:04 -0300

    modified data schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+//define data schema
 const UserSchema = new Schema({
-    name: String,
-    password: String
+    //add settings, the name and password must be unique and always required
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        unique: true,
+        required: true
+    },
 });
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
