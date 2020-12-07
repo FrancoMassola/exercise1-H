3d4399a804b47312a6fd22a7f4402ed247c8ddf5
[FrancoMassola] <frann.massola@gmail.com>
2020-12-06 22:03:39 -0300

    add routes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import Components
+import {ListComponent} from './components/list/list.component'
+import {SinginComponent} from './components/singin/singin.component'
+import {SingupComponent} from './components/singup/singup.component'
 
-const routes: Routes = [];
+//create application routers
+const routes: Routes = [
+{
+  //when the user path is visited, the component ListComponente is rendered
+  path: 'auth/login',
+  component: SinginComponent
+},
+{
+  //when the "root" path is visited, redirects to user path, with pathMatch: 'full' automatically
+  path: '',
+  redirectTo: '/auth/login',
+  pathMatch: 'full'
+},
+{
+  path: 'users',
+  component: ListComponent
+},
+{
+  path: 'singup',
+  component: SingupComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
