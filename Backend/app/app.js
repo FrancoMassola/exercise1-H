1e8e0c13059663cc6b2e90f97041b5ea4a6798fe
[FrancoMassola] <frann.massola@gmail.com>
2020-12-07 17:29:26 -0300

    add cors module to adds a header to the request allowing comunication with the server

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -3,10 +3,15 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
 const authRoute = require ('./routes/auth');
 const authToken = require('./middlewares/authToken');
+const cors = require('cors');
+
 
 //create express instance
 const App = express();
 App.use(express.urlencoded({ extended: false }));
+
+//this cors module adds a header to the request allowing communication with the server
+App.use(cors());
 
 //body-parser configuration
 App.use(bodyParser.json());
