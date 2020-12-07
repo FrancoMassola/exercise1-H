2d43ba3cbf483a92a9290c069af1e9c7d79b49b0
[FrancoMassola] <frann.massola@gmail.com>
2020-12-06 23:34:28 -0300

    import Forms Module

--- Frontend/src/app/components/singin/singin.component.ts
+++ Frontend/src/app/components/singin/singin.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singin.component.css']
 })
 export class SinginComponent implements OnInit {
+  //create an object to manipulate the loaded admin data  
+  admin = {
+    username: '',
+    password: ''
+  }
 
   constructor() { }
 
