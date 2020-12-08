ade4b33b1be8564f242199c6aac1595d3dc99665
[FrancoMassola] <frann.massola@gmail.com>
2020-12-08 16:42:03 -0300

    format code with prettier

--- Frontend/src/app/components/singin/singin.component.ts
+++ Frontend/src/app/components/singin/singin.component.ts
@@ -1,51 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 //import the service class
-import {AuthService} from '../../services/auth.service'
+import { AuthService } from '../../services/auth.service';
 //Import Router module
-import{Router} from '@angular/router'
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-singin',
   templateUrl: './singin.component.html',
-  styleUrls: ['./singin.component.css']
+  styleUrls: ['./singin.component.css'],
 })
 export class SinginComponent implements OnInit {
-  //create an object to manipulate the loaded admin data  
+  //create an object to manipulate the loaded admin data
   admin = {
     username: '',
-    password: ''
-  }
-//generate the instance of the class
-  constructor(private authService: AuthService,
-              private router: Router      
-    ) { }
+    password: '',
+  };
+  //generate the instance of the class
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async singIn(){
+  async singIn() {
     //admin object is sent to service
-    this.authService.singIn(this.admin)
-    .subscribe(
-      res =>{
-        console.log(res)
+    this.authService.singIn(this.admin).subscribe(
+      (res) => {
+        console.log(res);
         //validate navigation respect to the admin data
-        if (res.token == undefined){
+        if (res.token == undefined) {
+          this.router.navigate(['/users']);
+        } else {
+          //add token to localstorage
+          localStorage.setItem('token', res.token);
+          //redirect to
           this.router.navigate(['/users']);
         }
-        else{
-          //add token to localstorage
-        localStorage.setItem('token',res.token);
-        //redirect to 
-        this.router.navigate(['/users']);
-        }
-        
       },
-        err => console.log(err.error["message"])
-      
-    )
-    
+      (err) => console.log(err.error['message'])
+    );
   }
-
 }
